7ab7d17bbd665f5d82893dcd727d24cef24a380d
AnbhuSavi <saviprabhu123@gmail.com>
2017-07-10 17:44:54 +0530

    Create Alphabet123.java

---
+++ Alphabet123.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ 
+public class Alphabet123
+{
+   public static void main(String [] args) {
+new Alphabet();
+    
+    for ( int i = 65; i < 91; i ++ ) {
+    System.out.println("Upper Case\n"+(char)i + " " + (char)(i+32));
+    }
+    }
+
+}
